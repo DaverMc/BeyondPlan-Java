8ffbf45541ab8b058854c3c90a304eda45bad14e
Daver <dennis.fleer@outlook.de>
2024-04-05 18:09:36 +0200

    Configured build.gradle's Onley JsonParser Test are failing

--- BeyondPlan-Client/src/test/java/de/daver/beyondplan/client/web/WebClientTest.java
+++ BeyondPlan-Client/src/test/java/de/daver/beyondplan/client/web/WebClientTest.java
@@ -1,5 +1,7 @@
 package de.daver.beyondplan.client.web;
 
+import de.daver.beyondplan.core.web.WebServer;
+import de.daver.beyondplan.core.web.WebServerSocket;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,9 +10,13 @@ class WebClientTest {
 
     @Test
     void WebClientSocketStartUp() {
+        WebServerSocket serverSocket = assertDoesNotThrow(() -> new WebServerSocket(1337), "Test failed caused by outside factors");
+        serverSocket.start();
+
         WebClientSocket socket = assertDoesNotThrow(() ->  new WebClientSocket("localhost", 1337));
         assertTrue(socket.start());
         socket.handShake();
         assertDoesNotThrow(() -> socket.stop(false));
+        serverSocket.stop(false);
     }
 }
